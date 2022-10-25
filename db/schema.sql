d3189351b633da2c4ff937f40b75c5326043be9c
Neuy <neuwirth.andrew9@gmail.com>
2022-10-25 19:57:24 -0400

    finsished db

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,25 @@ USE meathead_db;
 
 CREATE TABLE accountInfo (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL
+    username VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE maxInfo (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (maxID) REFERENCES accountInfo(id)
+    maxID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (accountID) REFERENCES accountInfo(id)
     ON DELETE SET NULL,
+    accountID INT,
     maxBench INT,
     maxRepWeight INT,
     maxReps INT
 );
 
 CREATE TABLE excersiseTracking (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FOREIGN KEY (excersiseID) REFERENCES maxInfo(id)
+    excersiseID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (maxID) REFERENCES maxInfo(maxID)
     ON DELETE SET NULL,
+    maxID INT,
     benchPress INT,
     tricepExtensions INT,
     chestFlies INT
