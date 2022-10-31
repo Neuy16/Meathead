fecfcf96654c39fd9a822837d1da09bcf37f2464
DakMorg <108765820+DakMorg@users.noreply.github.com>
2022-10-30 21:51:10 -0400

    mostly completed front end and logic

--- server.js
+++ server.js
@@ -21,8 +21,9 @@ initializePassport(
 
 //database info goes here
 //right now users stores an empty array that is refreshed every time the server restarts
-const users = []
-//
+
+// const users = []
+
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -36,8 +37,38 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOveride('_method'))
 
+//sets input as userInputMax
+var userInputMax = 225
+
+//converts user max to a percentage and uses Math.round to round it to the nearest 5
+
+//bench set conversions
+var benchSet1 = Math.round(userInputMax * .6 / 5) * 5
+var benchSet2 = Math.round(userInputMax * .7 / 5) * 5
+var benchSet3 = Math.round(userInputMax * .8 / 5) * 5
+var benchSet4 = Math.round(userInputMax * .85 / 5) * 5
+var benchSet5 = Math.round(userInputMax * .9 / 5) * 5
+
+//tricep extension set conversions
+var triSet1 = Math.round(userInputMax * .13 / 5) * 5
+var triSet2 = Math.round(userInputMax * .13 / 5) * 5
+var triSet3 = Math.round(userInputMax * .15 / 5) * 5
+var triSet4 = Math.round(userInputMax * .15 / 5) * 5
+var triSet5 = Math.round(userInputMax * .17 / 5) * 5
+
+//chest fly set conversions
+var flySet1 = Math.round(userInputMax * .1 / 5) * 5
+var flySet2 = Math.round(userInputMax * .1 / 5) * 5
+var flySet3 = Math.round(userInputMax * .13 / 5) * 5
+var flySet4 = Math.round(userInputMax * .13 / 5) * 5
+var flySet5 = Math.round(userInputMax * .15 / 5) * 5
+
 app.get('/', checkAuthenticated, (req,res) => {
-    res.render('index.ejs', { name: 'Rick' })
+    res.render('index.ejs', 
+    { bset1: benchSet1, bset2: benchSet2, bset3: benchSet3, bset4: benchSet4, bset5: benchSet5,
+    tset1: triSet1, tset2: triSet2, tset3: triSet3, tset4: triSet4, tset5: triSet5,
+    fset1: flySet1, fset2: flySet2, fset3: flySet3, fset4: flySet4, fset5: flySet5
+    })
 })
 
 app.get('/login', checkNotAuthenticated, (req,res) => {
@@ -58,11 +89,11 @@ app.post('/register', checkNotAuthenticated, async (req,res) => {
  try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
 //not needed after database is connected
-    users.push({
+/*     users.push({
         id: Date.now().toString(),
         username: req.body.username,
         password: hashedPassword
-    })
+    }) */
 //
 res.redirect('/login')
  } catch {
